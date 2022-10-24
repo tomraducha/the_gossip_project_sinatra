800e2021a61047a0c0713a3036d2fee109255380
Thomas Raducha <thomasraducha@gmail.com>
2022-10-24 07:40:54 +0200

    first commit

---
+++ Gemfile
@@ -0,0 +1,8 @@
+source 'https://rubygems.org'
+ruby '2.7.4'
+gem 'pry'
+gem 'rspec'
+gem 'rubocop', '~> 1.36.0'
+gem 'sinatra'
+gem 'puma'
+gem 'shotgun'
---
+++ lib/controller.rb
@@ -0,0 +1,13 @@
+class ApplicationController < Sinatra::Base
+  get '/' do
+    erb :index
+  end
+
+  get '/gossips/new/' do
+    'Hello World'
+  end
+
+  post '/gossips/new/' do
+    puts 'Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal'
+  end
+end
