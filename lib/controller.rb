37b56ce24ffccaedcc5f310c9237e8f7327ba160
Thomas Raducha <thomasraducha@gmail.com>
2022-10-24 19:20:42 +0200

    last save

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,17 +1,30 @@
+require 'gossip'
+
 class ApplicationController < Sinatra::Base
   get '/' do
-    erb :index
+    erb :index, locals: { gossips: Gossip.all }
   end
 
-  get '/gossips/new/' do
-    'Hello World'
-  end
-
-  post '/gossips/new/' do
-    puts 'Ce programme ne fait rien pour le moment, on va donc afficher un message dans le terminal'
-  end
-
+  # Affiche la page html '/gossips/new/' sur laquelle il y a le formulaire.
   get '/gossips/new/' do
     erb :new_gossip
+  end
+
+  # Récupère les données du formulaire de la page et les renvoie à la database (db) html '/gossips/new/'.
+  post '/gossips/new/' do
+    Gossip.new("#{params['gossip_author']}", "#{params['gossip_content']}").save
+    redirect '/'
+  end
+
+  get '/hello/:id' do
+    # matches "GET /hello/foo" and "GET /hello/bar"
+    # params['name'] is 'foo' or 'bar'
+    "Hello #{params['name']}!"
+  end
+
+  # emande à la page show de définir une variable (gossip) qui sera le résultat de gossip.find, params sert à récupérer de façon temporaire,
+  # ce qui a été écrit avant un rechargement, id est le numéro qui sera incrémenté dans l'index et réutilisé pour créer de nouvelles URL.
+  get '/gossips/:id/' do
+    erb :show, locals: { gossip: Gossip.find(params['id'].to_i), id: params['id'] }
   end
 end
--- lib/gossip.rb
+++ lib/gossip.rb
@@ -9,24 +9,26 @@ class Gossip
   end
 
   def save
+    # Chaque élément de CSV.open  (dans gossip.csv)
     CSV.open('/home/thomas/Bureau/THE-HACKING-PROJECT/the_gossip_project_sinatra/db/gossip.csv', 'ab') do |csv|
       csv << [@author, @content]
     end
   end
 
+  # Sert a voir tous les gossips et à les retourner sous forme d'array
   def self.all
     all_gossips = []
-    CSV.read('/home/thomas/Bureau/THE-HACKING-PROJECT/the_gossip_project_sinatra/db/gossip.csv').each do |csv_line|
+    CSV.read('./db/gossip.csv').each do |csv_line|
       all_gossips << Gossip.new(csv_line[0], csv_line[1])
     end
     return all_gossips
   end
 
   def self.find(id)
-    data_base = CSV.read('/home/thomas/Bureau/THE-HACKING-PROJECT/the_gossip_project_sinatra/db/gossip.csv')
-    return data_base[id]
+    select = []
+    Gossip.all.each do |i|
+      select << i
+    end
+    select[id]
   end
 end
-
-my_gossip = Gossip.new('Mon super auteur', 'Ma super description')
-my_gossip.save
